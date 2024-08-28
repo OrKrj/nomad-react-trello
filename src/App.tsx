be979a3e8010010c3e20d5aac353076d4cc362c8
Zun <k5orkrj@gmail.com>
2024-08-28 11:08:54 +0900

    feat: add drag-and-drop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,30 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>one</li>}
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    // handle은 특정 영역에서만 드래그 가능
+                    // 여기선 draggableProps도 같이 적용되어 전체 영역 드래그 가능
+                    {...provided.dragHandleProps}
+                  >
+                    one
+                  </li>
+                )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
-                {() => <li>two</li>}
+                {(provided) => (
+                  // handle은 특정 영역에서만 드래그 가능
+                  // span 영역에서 드래그 가능
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>🔥</span>
+                    two
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
