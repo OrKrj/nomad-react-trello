3d6d2a78e9be0a24789a8cabc306a2dd813e27b4
Zun <k5orkrj@gmail.com>
2024-08-29 23:15:01 +0900

    perf: optimize rendering by applying React.memo to prevent unnecessary re-renders

--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
@@ -14,6 +15,7 @@ interface IDraggableCardProps {
 }
 
 function DraggableCard({ toDo, index }: IDraggableCardProps) {
+  console.log(toDo);
   return (
     <Draggable key={toDo} draggableId={toDo} index={index}>
       {(provided) => (
@@ -29,4 +31,4 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
   );
 }
 
-export default DraggableCard;
+export default React.memo(DraggableCard);
