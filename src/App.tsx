c69db88e657d7007a2cd5485480da6cf33ef00c8
Zun <k5orkrj@gmail.com>
2024-08-29 22:22:11 +0900

    refactor: organize DraggableCard into components directory

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DraggableCard from "./Components/DraggableCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,19 +33,13 @@ const Board = styled.div`
   border-radius: 5px;
 `;
 
-const Card = styled.div`
-  background-color: ${(props) => props.theme.cardColor};
-  padding: 10px;
-  margin-bottom: 10px;
-`;
-
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
     if (!destination) return;
 
     setToDos((oldToDos) => {
-      // non-mutation을 위한 copy
+      // non-mutation을 위한 copy // splice 메소드는 mutation
       const toDosCopy = [...oldToDos];
       // 1) Delete item on source.index(delete dragged item)
       toDosCopy.splice(source.index, 1);
@@ -62,17 +57,7 @@ function App() {
             {(provided) => (
               <Board ref={provided.innerRef} {...provided.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(provided) => (
-                      <Card
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
+                  <DraggableCard key={toDo} index={index} toDo={toDo} />
                 ))}
                 {/* placeholder 덕분에 드래그시 Board 크기가 변하지 않음 */}
                 {provided.placeholder}
---
+++ src/Components/DraggableCard.tsx
@@ -0,0 +1,31 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+interface IDraggableCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DraggableCard({ toDo, index }: IDraggableCardProps) {
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(provided) => (
+        <Card
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default DraggableCard;
